3b2d9b2fa34ff8aaafd9617f25a312a54ef7e2ba
Volkan <volkanarisli@gmail.com>
2021-02-07 19:10:17 +0300

    initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.prototype = {
+    title: PropTypes.string.isRequired
+}
+//css in js
+// const headingStyle = {
+//     color: 'red'
+// }
+
+export default Header
